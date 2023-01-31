24e9b3776c45028763778c5c63ec2c7bfa32ce87
Jerry Janquart <jerryjanquart@Jerrys-iMac.attlocal.net>
2023-01-31 12:41:20 -0600

    add github link to footer

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="container-fluid fixed-bottom">
+    <div class="row bottom-space text-center" ><a href="https://github.com/githubbjar/portfolio" target="_blank" class="github">Github</a></div>
+</div>
--- projects/index.php
+++ projects/index.php
@@ -147,9 +147,8 @@
 
 
 
-        <div class="container-fluid fixed-bottom">
-            <div class="row bottom-space"> 
-            </div>
+        <?php include '../footer.php'; ?>
+
         </div>  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
   </body>
